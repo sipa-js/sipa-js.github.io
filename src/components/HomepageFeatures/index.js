99e811dafcc12351989a21baf5b5a2505ce2a65e
magynhard <github@mail.magynhard.de>
2023-01-22 14:17:31 +0100

    add basic content to home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,35 +1,78 @@
 import React from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Particularly Simple',
-    Svg: require('@site/static/img/pages/home/js_html_css.svg').default,
+    title: 'Current javascript framework hype',
+    Svg: require('@site/static/img/pages/home/angular_react_vue.svg').default,
     description: (
       <>
-          Simpartic tries to stick as close as possible to the original HTML
-          standard. If you know HTML and JavaScript, you already know Simpartic.
+          Have you also wondered why more and more big JavaScript frameworks have appeared in the last few years, often sponsored by well-known companies and offering somehow interesting concepts, but not having much in common with the original JavaScript?
+          <br/><br/>
+          Did you have the impression that completely new worlds were developed there, as if you had to learn a new language?
+          <br/><br/>
+          Did you have the impression that development used to be much easier and more intuitive?
+          <br/><br/>
+          Does it annoy you that you write code that has not much to do with the code that is executed in the browser? Would you rather just be able to run and debug your code in the browser?
+          <br/><br/>
+          So have you considered maybe just building your own framework again?
+          <br/><br/>
+          If you find yourself somewhere in the questions here, it could be that you might really like Simpartic.
+          <br/><br/>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      title: 'Why Simpartic?',
+      Svg: require('@site/static/img/pages/home/js_html_css.svg').default,
+      description: (
+          <>
+              I just want to develop with Javascript: The code i write IS the code i run in the browser.
+              <br/><br/>
+              Writing Javascript and HTML the old way! So I can directly run and debug my code in the browser. Instant!
+              <br/><br/>
+              Intensive transpiling (minifying) will (can!) finally happen when creating a production build. I even can create a single file production build out of the box! (Feature not yet implemented)
+              <br/><br/>
+              I don't want to learn a new framework world.
+              I can JavaScript, I want to do JavaScript. I want it simple, I want it stupid easy. So i can add whatever I want if I need to.
+              <br/><br/>
+              A framework that gives me a frame, but doesn't force me into it.
+              <br/><br/>
+              ... and at the end I want to be a happy programmer! 😊
+          </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Main features',
+    Svg: require('@site/static/img/pages/home/simpartic_features.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+          <ul style={{textAlign: "left"}}>
+              <li>Solid basic project structure</li>
+              <li>Simple but powerful page pattern structure with loader / router</li>
+              <li>Live development web server for JavaScript and SCSS</li>
+              <li>Life cycle with hook feature</li>
+              <li>Internationalization out of the box ![State](https://img.shields.io/badge/-todo-blue)</li>
+              <li>Global state storage to keep state on three different persistence levels</li>
+              <li>Builder to create compressed and minified .js and .css for production</li>
+              <li>Optionally:
+                <ul>
+                    <li>EJS template engine (optional) ![State](https://img.shields.io/badge/-prepared-blue)</li>
+                    <li>Good old jQuery</li>
+                </ul>
+              </li>
+          </ul>
+          <br/>
+          To get an overview and idea about the framework, have a look at <div className={styles.buttons}>
+          <Link
+              className="button button--secondary button--lg"
+              to="/docs/guides/intro">
+              Getting started ⏱️
+          </Link>
+      </div>
       </>
     ),
   },
