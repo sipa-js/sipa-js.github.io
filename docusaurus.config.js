0785bd3896df38a154642edcbda5bcdf69f4b6e2
magynhard <github@mail.magynhard.de>
2023-05-16 17:11:02 +0200

    reformat indention

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,128 +6,128 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Simpartic',
-  tagline: '!!! <under heavy construction> !!!',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/simpartic/favicon.svg',
+    title: 'Simpartic',
+    tagline: '!!! <under heavy construction> !!!',
+    url: 'https://your-docusaurus-test-site.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/simpartic/favicon.svg',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'magynhard', // Usually your GitHub org/user name.
-  projectName: 'simpartic.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
-  trailingSlash: false,
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'magynhard', // Usually your GitHub org/user name.
+    projectName: 'simpartic.github.io', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
+    trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/simpartic/simpartic.github.io/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/simpartic/simpartic.github.io/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    {
-      navbar: {
-        title: 'SIMPARTIC',
-        logo: {
-          alt: 'My Site SIMPARTIC',
-          src: 'img/simpartic/favicon.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'guides/overview',
-            position: 'left',
-            label: 'Guides',
-          },
-          {
-            type: 'doc',
-            docId: 'api/api',
-            position: 'left',
-            label: 'API',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/simpartic/simpartic.github.io',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'News',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/simpartic',
-              }
-            ],
-          },
-          {
-            title: 'Network',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/simpartic/simpartic.github.io',
-              },
-            ],
-          },
-        ],
-        copyright: `<small style="color: #ffc005">Copyright © ${new Date().getFullYear()} by Contributors (LGPLv3). Built with Docusaurus.</small>`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      },
-      colorMode: {
-        defaultMode: 'dark',
-        disableSwitch: false,
-        respectPrefersColorScheme: true,
-      }
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
     },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/simpartic/simpartic.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/simpartic/simpartic.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        {
+            navbar: {
+                title: 'SIMPARTIC',
+                logo: {
+                    alt: 'My Site SIMPARTIC',
+                    src: 'img/simpartic/favicon.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'guides/overview',
+                        position: 'left',
+                        label: 'Guides',
+                    },
+                    {
+                        type: 'doc',
+                        docId: 'api/api',
+                        position: 'left',
+                        label: 'API',
+                    },
+                    {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href: 'https://github.com/simpartic/simpartic.github.io',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'News',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Stack Overflow',
+                                href: 'https://stackoverflow.com/questions/tagged/simpartic',
+                            }
+                        ],
+                    },
+                    {
+                        title: 'Network',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/simpartic/simpartic.github.io',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `<small style="color: #ffc005">Copyright © ${new Date().getFullYear()} by Contributors (LGPLv3). Built with Docusaurus.</small>`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme
+            },
+            colorMode: {
+                defaultMode: 'dark',
+                disableSwitch: false,
+                respectPrefersColorScheme: true,
+            }
+        },
 };
 
 module.exports = config;
