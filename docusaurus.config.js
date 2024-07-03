40afde5b89cbf698dc0f0c0938e686295e31cd94
magynhard <github@mail.magynhard.de>
2024-07-03 10:32:04 +0200

    uppercase title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Sipa',
+    title: 'SIPA',
     tagline: 'documentation under construction (BETA)',
     url: 'https://sipa-js.github.io/',
     baseUrl: '/',
